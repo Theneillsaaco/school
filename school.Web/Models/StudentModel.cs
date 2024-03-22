7278e2ec7899d06bac0d7a0668bc205f186572a7
Isaac De peña <isaacdepena18@gmail.com>
2024-03-22 18:19:51 -0400

    a

--- school.Web/Models/StudentModel.cs
+++ school.Web/Models/StudentModel.cs
@@ -1,6 +1,25 @@
-﻿namespace school.Web.Models
+﻿using School.DAL.Models;
+
+namespace school.Web.Models
 {
     public class StudentModel
     {
+        public StudentModel()
+        {
+
+        }
+        public StudentModel(StudentDaoModel studentDaoModel)
+        {
+            this.Id = studentDaoModel.Id;
+            this.CreationDate = studentDaoModel.CreationDate;
+            this.LastName = studentDaoModel.LastName;
+            this.FirstName = studentDaoModel.FirstName;
+            this.EnrollmentDate = studentDaoModel.EnrollmentDate;
+        }
+        public int Id { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
     }
 }
