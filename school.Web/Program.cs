5db555bf4e00a1f1ec202d75341bf6e689eac422
Isaac De peña <isaacdepena18@gmail.com>
2024-03-24 13:26:57 -0400

    a

--- school.Web/Program.cs
+++ school.Web/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddTransient<IDaoDepartment, DaoDepartment>();
 builder.Services.AddTransient<IDaoCourse, DaoCourse>();
 builder.Services.AddTransient<IDaoOnlineCourse, DaoOnlineCourse>();
 builder.Services.AddTransient<IDaoStudent, DaoStudent>();
+builder.Services.AddTransient<IDaoInstructor, DaoInstructor>();
 
 #endregion
 
